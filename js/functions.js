4a53c8ca9bb40c4c1b018bb4e73b3f8f95c89ce5
Wellington Rogério <wrogerio.ing@gmail.com>
2025-05-05 15:24:56 -0300

    .

--- js/functions.js
+++ js/functions.js
@@ -1,32 +1,3 @@
-const url_root = 'https://lqxnlojeuuextavhyfrd.supabase.co/rest/v1/'
-const louvores = 'TbLouvores'
-const letras = 'TbLetras'
-const anon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxxeG5sb2pldXVleHRhdmh5ZnJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0NjEyMTYsImV4cCI6MjA2MjAzNzIxNn0.Ig7zUn--bBj2YIYdFHaRoi-1kfHW2aWpKu6qRmqiDaw'
 
-function getLouvores() {
-  return new Promise((resolve, reject) => {
-    $.ajax({
-      url: `${url_root}${louvores}?select=*`,
-      type: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'apikey': anon,
-        'Authorization': `Bearer ${anon}`,
-      },
-      success: function (data) {
-        const arrayLouvores = data.map(louvor => ({
-          cantor: louvor.cantor,
-          formula: louvor.formula,
-          id: louvor.id,
-          nome: louvor.nome,
-          tom: louvor.tom
-        }));
-        resolve(arrayLouvores); // Retorna o array para quem chamou
-      },
-      error: function (xhr, status, error) {
-        console.error('Erro ao buscar os louvores:', error);
-        reject(error); // Retorna o erro para quem chamou
-      }
-    });
-  });
-}
+
+
--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
-// tableDataBody (tbody)
+const url_root = 'https://lqxnlojeuuextavhyfrd.supabase.co/rest/v1/'
+const louvores = 'TbLouvores'
+const letras = 'TbLetras'
+const anon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxxeG5sb2pldXVleHRhdmh5ZnJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0NjEyMTYsImV4cCI6MjA2MjAzNzIxNn0.Ig7zUn--bBj2YIYdFHaRoi-1kfHW2aWpKu6qRmqiDaw'
 
 $(function () {
+  $('#tableDataBody').empty();
   getLouvores()
     .then(louvores => {
-      console.log(louvores); // Verifica se os dados foram carregados corretamente
       louvores.forEach(louvor => {
         let row = `
           <tr>
@@ -20,3 +23,32 @@ $(function () {
       console.error('Erro ao carregar louvores:', error);
     });
 });
+
+
+function getLouvores() {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: `${url_root}${louvores}?select=*`,
+      type: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'apikey': anon,
+        'Authorization': `Bearer ${anon}`,
+      },
+      success: function (data) {
+        const arrayLouvores = data.map(louvor => ({
+          cantor: louvor.cantor,
+          formula: louvor.formula,
+          id: louvor.id,
+          nome: louvor.nome,
+          tom: louvor.tom
+        }));
+        resolve(arrayLouvores); // Retorna o array para quem chamou
+      },
+      error: function (xhr, status, error) {
+        console.error('Erro ao buscar os louvores:', error);
+        reject(error); // Retorna o erro para quem chamou
+      }
+    });
+  });
+}
