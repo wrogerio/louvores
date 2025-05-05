471b02d5d543a93fb6646d28f1385e563dfabba0
Wellington Rogério <wrogerio.ing@gmail.com>
2025-05-05 15:12:31 -0300

    .

--- js/functions.js
+++ js/functions.js
@@ -3,39 +3,30 @@ const louvores = 'TbLouvores'
 const letras = 'TbLetras'
 const anon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxxeG5sb2pldXVleHRhdmh5ZnJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0NjEyMTYsImV4cCI6MjA2MjAzNzIxNn0.Ig7zUn--bBj2YIYdFHaRoi-1kfHW2aWpKu6qRmqiDaw'
 
-
-
 function getLouvores() {
-    let url = `${url_root}${louvores}?select=*`;
-    fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'apikey': anon,
-            'Authorization': `Bearer ${anon}`,
-        }
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json();
-    })
-    .then(data => {
-        let louvores = data.map(louvor => {
-            return {
-                id: louvor.id,
-                nome: louvor.nome,
-                letra_id: louvor.letra_id,
-                letra: null // Inicialmente nulo, será preenchido depois
-            };
-        });
-        return louvores;
-    })
-    .then(louvores => {
-        console.log(louvores);
-    })
-    .catch(error => {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: `${url_root}${louvores}?select=*`,
+      type: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'apikey': anon,
+        'Authorization': `Bearer ${anon}`,
+      },
+      success: function (data) {
+        const arrayLouvores = data.map(louvor => ({
+          cantor: louvor.cantor,
+          formula: louvor.formula,
+          id: louvor.id,
+          nome: louvor.nome,
+          tom: louvor.tom
+        }));
+        resolve(arrayLouvores); // Retorna o array para quem chamou
+      },
+      error: function (xhr, status, error) {
         console.error('Erro ao buscar os louvores:', error);
+        reject(error); // Retorna o erro para quem chamou
+      }
     });
+  });
 }
--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
-$(document).ready(function() {
-  getLouvores();
+// tableDataBody (tbody)
+
+$(function () {
+  getLouvores()
+    .then(louvores => {
+      console.log(louvores); // Verifica se os dados foram carregados corretamente
+      louvores.forEach(louvor => {
+        let row = `
+          <tr>
+            <td>${louvor.nome}</td>
+            <td>${louvor.cantor}</td>
+            <td width=80>${louvor.tom}</td>
+            <td class="text-center"></td>
+          </tr>
+        `
+        $('#tableDataBody').append(row);
+      });
+    })
+    .catch(error => {
+      console.error('Erro ao carregar louvores:', error);
+    });
 });
