6486b7d752660e9f4104f04f92fd504232a7dc90
Wellington Rogério <wrogerio.ing@gmail.com>
2025-05-06 11:41:38 -0300

    .

--- js/index.js
+++ js/index.js
@@ -1,7 +1,4 @@
-const url_root = 'https://lqxnlojeuuextavhyfrd.supabase.co/rest/v1/'
-const louvores = 'TbLouvores'
-const letras = 'TbLetras'
-const anon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxxeG5sb2pldXVleHRhdmh5ZnJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0NjEyMTYsImV4cCI6MjA2MjAzNzIxNn0.Ig7zUn--bBj2YIYdFHaRoi-1kfHW2aWpKu6qRmqiDaw'
+import { getLouvores } from './functions.js'
 const tableDataBody = document.getElementById('tableDataBody')
 
 tableDataBody.innerHTML = ''
@@ -40,62 +37,3 @@ getLouvores().then(louvores => {
     tableDataBody.appendChild(row)
   })
 })
-
-function getLouvores() {
-  return new Promise((resolve, reject) => {
-    const url = `${url_root}${louvores}?select=*`;
-
-    // using fetch API to get data from the API
-    fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'apikey': anon,
-        'Authorization': `Bearer ${anon}`,
-      },
-    }).then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok' + response.statusText);
-      }
-      return response.json();
-    }).then(data => {
-      const arrayLouvores = data.map(louvor => ({
-        cantor: louvor.cantor,
-        formula: louvor.formula,
-        id: louvor.id,
-        nome: louvor.nome,
-        inicio: louvor.inicio,
-        tom: louvor.tom
-      }));
-      resolve(arrayLouvores); // Retorna o array para quem chamou
-    }).catch(error => {
-      console.error('Erro ao buscar os louvores:', error);
-      reject(error); // Retorna o erro para quem chamou
-    })
-
-    //   $.ajax({
-    //     url: `${url_root}${louvores}?select=*`,
-    //     type: 'GET',
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //       'apikey': anon,
-    //       'Authorization': `Bearer ${anon}`,
-    //     },
-    //     success: function (data) {
-    //       const arrayLouvores = data.map(louvor => ({
-    //         cantor: louvor.cantor,
-    //         formula: louvor.formula,
-    //         id: louvor.id,
-    //         nome: louvor.nome,
-    //         inicio: louvor.inicio,
-    //         tom: louvor.tom
-    //       }));
-    //       resolve(arrayLouvores); // Retorna o array para quem chamou
-    //     },
-    //     error: function (xhr, status, error) {
-    //       alert(error);
-    //       reject(error); // Retorna o erro para quem chamou
-    //     }
-    //   });
-  });
-}
