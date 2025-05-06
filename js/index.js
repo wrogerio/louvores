c383139e5b35b68adbbbb8875a6bd30cd3ec54f6
Wellington Rogério <wrogerio.ing@gmail.com>
2025-05-06 11:29:17 -0300

    .

--- js/index.js
+++ js/index.js
@@ -9,30 +9,60 @@ getLouvores()
 
 function getLouvores() {
   alert('getLouvores')
-  // return new Promise((resolve, reject) => {
-  //   $.ajax({
-  //     url: `${url_root}${louvores}?select=*`,
-  //     type: 'GET',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       'apikey': anon,
-  //       'Authorization': `Bearer ${anon}`,
-  //     },
-  //     success: function (data) {
-  //       const arrayLouvores = data.map(louvor => ({
-  //         cantor: louvor.cantor,
-  //         formula: louvor.formula,
-  //         id: louvor.id,
-  //         nome: louvor.nome,
-  //         inicio: louvor.inicio,
-  //         tom: louvor.tom
-  //       }));
-  //       resolve(arrayLouvores); // Retorna o array para quem chamou
-  //     },
-  //     error: function (xhr, status, error) {
-  //       alert(error);
-  //       reject(error); // Retorna o erro para quem chamou
-  //     }
-  //   });
-  // });
+  return new Promise((resolve, reject) => {
+    const url = `${url_root}${louvores}?select=*`;
+
+    // using fetch API to get data from the API
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'apikey': anon,
+        'Authorization': `Bearer ${anon}`,
+      },
+    }).then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok' + response.statusText);
+      }
+      return response.json();
+    }).then(data => {
+      const arrayLouvores = data.map(louvor => ({
+        cantor: louvor.cantor,
+        formula: louvor.formula,
+        id: louvor.id,
+        nome: louvor.nome,
+        inicio: louvor.inicio,
+        tom: louvor.tom
+      }));
+      resolve(arrayLouvores); // Retorna o array para quem chamou
+    }).catch(error => {
+      console.error('Erro ao buscar os louvores:', error);
+      reject(error); // Retorna o erro para quem chamou
+    })
+
+    //   $.ajax({
+    //     url: `${url_root}${louvores}?select=*`,
+    //     type: 'GET',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //       'apikey': anon,
+    //       'Authorization': `Bearer ${anon}`,
+    //     },
+    //     success: function (data) {
+    //       const arrayLouvores = data.map(louvor => ({
+    //         cantor: louvor.cantor,
+    //         formula: louvor.formula,
+    //         id: louvor.id,
+    //         nome: louvor.nome,
+    //         inicio: louvor.inicio,
+    //         tom: louvor.tom
+    //       }));
+    //       resolve(arrayLouvores); // Retorna o array para quem chamou
+    //     },
+    //     error: function (xhr, status, error) {
+    //       alert(error);
+    //       reject(error); // Retorna o erro para quem chamou
+    //     }
+    //   });
+  });
 }
