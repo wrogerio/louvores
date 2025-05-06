a018446603d930ba444faff5e6efadeb53cf1c79
Wellington Rogério <wrogerio.ing@gmail.com>
2025-05-06 11:32:30 -0300

    .

--- js/index.js
+++ js/index.js
@@ -5,10 +5,43 @@ const anon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZ
 const tableDataBody = document.getElementById('tableDataBody')
 
 tableDataBody.innerHTML = ''
-getLouvores()
+getLouvores().then(louvores => {
+  louvores.forEach(louvor => {
+    const row = document.createElement('tr')
+    row.innerHTML = `
+      <tr>
+        <td colspan="3">
+          <div class="d-flex justify-content-between align-items-center">
+            <a href="apresentacao.html?id=${louvor.id}" class="text-primary me-2">
+              ${louvor.nome}
+            </a>
+            <section class='text-success fw-bold'>
+              <span class='me-2'>${louvor.tom}</span>  
+              <span>${louvor.formula}</span>  
+            </section>
+          </div>
+          <div class="d-flex justify-content-between align-items-center">
+            <small class="text-muted">${louvor.cantor}</small>
+            <section class='text-success fw-bold'>
+              <a href="editar.html?id=${louvor.id}" class="text-primary me-2">
+                <i class="fa-regular fa-money-check-pen"></i>
+              </a>
+              <a href="remover.html?id=${louvor.id}" class="text-danger">
+                <i class="fa-solid fa-trash-xmark"></i>
+              </a>
+            </section>
+          </div>
+          <div class="d-flex justify-content-start align-items-center">
+            <small class="text-success">${louvor.inicio}</small>
+          </div>
+        </td>
+      </tr>
+    `
+    tableDataBody.appendChild(row)
+  })
+})
 
 function getLouvores() {
-  alert('getLouvores')
   return new Promise((resolve, reject) => {
     const url = `${url_root}${louvores}?select=*`;
 
