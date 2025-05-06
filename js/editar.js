bf7cbdd69198bc64d69f36743278a148e9a9077b
Wellington Rogério <wrogerio.ing@gmail.com>
2025-05-06 11:48:12 -0300

    .

--- js/editar.js
+++ js/editar.js
@@ -1,24 +1,18 @@
-const url_root = 'https://lqxnlojeuuextavhyfrd.supabase.co/rest/v1/'
-const louvores = 'TbLouvores'
-const letras = 'TbLetras'
-const anon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxxeG5sb2pldXVleHRhdmh5ZnJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0NjEyMTYsImV4cCI6MjA2MjAzNzIxNn0.Ig7zUn--bBj2YIYdFHaRoi-1kfHW2aWpKu6qRmqiDaw'
+import { getLouvorById, getUrlId, editarLouvor } from './functions.js'
 
 $(function () {
-
-  $(window).on('load', function () {
-    const id = getUrlId();
-    getLouvorById(id)
-      .then(louvor => {
-        $('#nome').val(louvor.nome);
-        $('#cantor').val(louvor.cantor);
-        $('#tom').val(louvor.tom);
-        $('#formula').val(louvor.formula);
-        $('#inicio').val(louvor.inicio);
-      })
-      .catch(error => {
-        console.error('Erro ao carregar louvor:', error);
-      });
-  });
+  const id = getUrlId();
+  getLouvorById(id)
+    .then(louvor => {
+      $('#nome').val(louvor.nome);
+      $('#cantor').val(louvor.cantor);
+      $('#tom').val(louvor.tom);
+      $('#formula').val(louvor.formula);
+      $('#inicio').val(louvor.inicio);
+    })
+    .catch(error => {
+      console.error('Erro ao carregar louvor:', error);
+    });
 
 
   $('#salvar').on('click', function (e) {
@@ -37,7 +31,7 @@ $(function () {
 
     editarLouvor(id, nome, cantor, inicio, tom, formula)
       .then(() => {
-        window.location.href = 'index.html'; 
+        window.location.href = 'index.html';
       })
       .catch(error => {
         console.error('Erro ao editar louvor:', error);
@@ -58,59 +52,3 @@ $(function () {
     });
   });
 });
-
-function getUrlId() {
-  const urlParams = new URLSearchParams(window.location.search);
-  return urlParams.get('id');
-}
-
-function getLouvorById(id) {
-  return new Promise((resolve, reject) => {
-    $.ajax({
-      url: `${url_root}${louvores}?select=*`,
-      type: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'apikey': anon,
-        'Authorization': `Bearer ${anon}`,
-      },
-      success: function (data) {
-        const louvor = data.find(louvor => louvor.id === id);
-        resolve(louvor); // Retorna o louvor encontrado
-      },
-      error: function (xhr, status, error) {
-        console.error('Erro ao buscar o louvor:', error);
-        reject(error); // Retorna o erro para quem chamou
-      }
-    });
-  });
-}
-
-function editarLouvor(id, nome, cantor, inicio, tom, formula) {
-  return new Promise((resolve, reject) => {
-    $.ajax({
-      url: `${url_root}${louvores}?id=eq.${id}`,
-      type: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-        'apikey': anon,
-        'Authorization': `Bearer ${anon}`,
-        'Prefer': 'return=minimal'
-      },
-      data: JSON.stringify({
-        nome: nome,
-        cantor: cantor,
-        inicio: inicio,
-        tom: tom,
-        formula: formula
-      }),
-      success: function () {
-        resolve(true);
-      },
-      error: function (xhr, status, error) {
-        console.error('Erro ao editar o louvor:', error);
-        reject(error); // Retorna o erro para quem chamou
-      }
-    });
-  });
-}
--- js/index.js
+++ js/index.js
@@ -1,39 +1,40 @@
 import { getLouvores } from './functions.js'
-const tableDataBody = document.getElementById('tableDataBody')
 
-tableDataBody.innerHTML = ''
-getLouvores().then(louvores => {
-  louvores.forEach(louvor => {
-    const row = document.createElement('tr')
-    row.innerHTML = `
-      <tr>
-        <td colspan="3">
-          <div class="d-flex justify-content-between align-items-center">
-            <a href="apresentacao.html?id=${louvor.id}" class="text-primary me-2">
-              ${louvor.nome}
-            </a>
-            <section class='text-success fw-bold'>
-              <span class='me-2'>${louvor.tom}</span>  
-              <span>${louvor.formula}</span>  
-            </section>
-          </div>
-          <div class="d-flex justify-content-between align-items-center">
-            <small class="text-muted">${louvor.cantor}</small>
-            <section class='text-success fw-bold'>
-              <a href="editar.html?id=${louvor.id}" class="text-primary me-2">
-                <i class="fa-regular fa-money-check-pen"></i>
+$(function () {
+  $('#tableDataBody').empty()
+  getLouvores().then(louvores => {
+    louvores.forEach(louvor => {
+      $('#tableDataBody').append(`
+        <tr>
+          <td colspan="3">
+            <div class="d-flex justify-content-between align-items-center">
+              <a href="apresentacao.html?id=${louvor.id}" class="text-primary me-2">
+                ${louvor.nome}
               </a>
-              <a href="remover.html?id=${louvor.id}" class="text-danger">
-                <i class="fa-solid fa-trash-xmark"></i>
-              </a>
-            </section>
-          </div>
-          <div class="d-flex justify-content-start align-items-center">
-            <small class="text-success">${louvor.inicio}</small>
-          </div>
-        </td>
-      </tr>
-    `
-    tableDataBody.appendChild(row)
-  })
+              <section class='text-success fw-bold'>
+                <span class='me-2'>${louvor.tom}</span>  
+                <span>${louvor.formula}</span>  
+              </section>
+            </div>
+            <div class="d-flex justify-content-between align-items-center">
+              <small class="text-muted">${louvor.cantor}</small>
+              <section class='text-success fw-bold'>
+                <a href="editar.html?id=${louvor.id}" class="text-primary me-2">
+                  <i class="fa-regular fa-money-check-pen"></i>
+                </a>
+                <a href="remover.html?id=${louvor.id}" class="text-danger">
+                  <i class="fa-solid fa-trash-xmark"></i>
+                </a>
+              </section>
+            </div>
+            <div class="d-flex justify-content-start align-items-center">
+              <small class="text-success">${louvor.inicio}</small>
+            </div>
+          </td>
+        </tr>
+      `)
+    })
+  }).catch(error => {
+      console.error('Erro ao carregar louvores:', error);
+    });
 })
